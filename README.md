7ac7356268fd170f36f37bc5140df1da39f367c4
rahulkhichar7 <rahulkhichar531@gmail.com>
2026-04-18 23:40:48 +0530

    Updated utils file

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Paper: https://www.actuariesindia.org/sites/default/files/2022-05/DVPCPT_Dheeraj
 
 Trading Cost: https://zerodha.com/charges/#tab-equities
 
+## Assumptions
+- No slippage/ exicution delay. To add it in cost, modify the spread_cost parameter in config.py 
+- Best lending rates and borrowing rates(SBI) are taken but this may depend on your source of fund & civil score.
---
+++ config.py
@@ -0,0 +1,30 @@
+# Brokerage
+tr = 0.0003     # 0.03%
+B = 20          # cap per leg
+
+# Transaction charges
+theta_c = 0.0003553
+theta_p = 0.0003553
+theta_s = 0.0000307
+
+# Taxes
+gst = 0.18
+
+# STT (sell side only)
+tau_c_sell = 0.0015
+tau_p_sell = 0.0015
+tau_s = 0.00025
+
+# Margin
+alpha = 0.093
+beta = 0.02
+
+# Interest rates
+rb = 0.115
+rl = 0.06
+
+# Spread: Slippage & Delay in exicution
+spread_cost = 0
+
+# Lot size
+N = 65
